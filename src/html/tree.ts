cd75c32593b2a9a6df85f385efac01df381d7f70
Discusser <47938380+Discusser@users.noreply.github.com>
2024-09-23 21:26:46 +0200

    Move printing to a method

--- src/html/tree.ts
+++ src/html/tree.ts
@@ -57,4 +57,14 @@ export class HTMLTree {
   constructor() {
     this.root = null;
   }
+
+  printTree() {
+    let toVisit: Array<[HTMLElement, number]> = [[this.root, 0]];
+
+    while (toVisit.length > 0) {
+      let [visiting, depth] = toVisit.pop();
+      toVisit = toVisit.concat(visiting.children.reverse().map((e) => [e, depth + 1]));
+      console.log("  ".repeat(depth) + visiting);
+    }
+  }
 }
--- src/website_generator.ts
+++ src/website_generator.ts
@@ -1,15 +1,7 @@
 import { readFileSync } from "fs";
 import { HTMLParser } from "./html/parser.js";
-import { HTMLElement } from "./html/tree.js";
 
 const buf = readFileSync("./test/src/index.html");
 
 const tree = HTMLParser.parseString(buf.toString());
-let toVisit: Array<[HTMLElement, number]> = [[tree.root, 0]];
-console.log(buf.toString());
-
-while (toVisit.length > 0) {
-  let [visiting, depth] = toVisit.pop();
-  toVisit = toVisit.concat(visiting.children.reverse().map((e) => [e, depth + 1]));
-  console.log("  ".repeat(depth) + "" + visiting);
-}
+tree.printTree();
